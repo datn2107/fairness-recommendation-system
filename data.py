43a2b4301954bb828d917b9da8616576dae93b47
datn2107 <datn2107@gmail.com>
2024-02-26 09:04:29 +0700

    Add implementation of metrics

--- data.py
+++ data.py
@@ -2,38 +2,6 @@ import numpy as np
 from abc import ABC, abstractmethod
 
 
-def create_rank_matrix(S: np.ndarray) -> np.ndarray:
-    """
-    Create the rank matrix based on the input matrix S.
-
-    Parameters:
-    S (np.ndarray): The predicted score matrix of shape (n_users, n_items).
-
-    Returns:
-    np.ndarray: The matrix indicate the rank of each item for each user (n_users, n_items).
-    """
-    rank = np.zeros_like(S, dtype=np.int32)
-    ind_matrix = np.argsort(S, axis=1)[:, ::-1]
-    rank[np.arange(S.shape[0])[:, np.newaxis], ind_matrix] = np.tile(
-        np.arange(1, S.shape[1] + 1), (S.shape[0], 1)
-    )
-
-    return rank
-
-
-def create_top_item_matrix(S: np.ndarray) -> np.ndarray:
-    """
-    Calculate the user rank matrix based on the input matrix S.
-
-    Parameters:
-    S (np.ndarray): The predicted score matrix of shape (n_users, n_items).
-
-    Returns:
-    np.ndarray: The matrix indicate top items for each user (n_users, n_items).
-    """
-    return np.argsort(S, axis=1)[:, ::-1]
-
-
 class DataConversionStrategy(ABC):
     @abstractmethod
     def convert_to_relevance_matrix(
@@ -75,7 +43,7 @@ class RankMatrixConversionStrategy(DataConversionStrategy):
         else:
             relevance_matrix[data <= k] = 1
 
-        return relevance_matrix
+        return relevance_matrix.astype(np.int32)
 
 
 class TopItemMatrixConversionStrategy(DataConversionStrategy):
@@ -98,19 +66,20 @@ class TopItemMatrixConversionStrategy(DataConversionStrategy):
 
         ind = np.arange(relevance_matrix.shape[0])[:, np.newaxis]
         if rank_relevance:
-            relevance_matrix[ind, data] = n_item - np.tile(
-                np.arange(n_item), (relevance_matrix.shape[0], 1)
+            relevance_matrix[ind, data] = (
+                n_item - 1 - np.tile(np.arange(n_item), (relevance_matrix.shape[0], 1))
             )
         else:
             relevance_matrix[ind, data[:, :k]] = 1
 
-        return relevance_matrix
+        return relevance_matrix.astype(np.int32)
 
 
-class DataConversionContext:
+class DataConversion:
     """
     Strategy Pattern Design for Data Conversion.
     """
+
     DataConvertStrategy = None
 
     def __init__(self, strategy: DataConversionStrategy = None):
@@ -126,26 +95,80 @@ class DataConversionContext:
             data, k, rank_relevance
         )
 
+    @staticmethod
+    def convert_rank_relevance_matrix_to_binary(
+        data: np.ndarray, k: int = 30
+    ) -> np.ndarray:
+        """
+        Convert the rank relevance matrix to binary relevance matrix based on the input relevance matrix R.
 
-if __name__ == '__main__':
+        Parameters:
+        data (np.ndarray): The matrix indicate the rank of each item for each user (n_users, n_items).
+        k (int): The number of items to consider for each user. Default is 30.
+
+        Returns:
+        np.ndarray: The binary relevance score matrix of shape (n_users, n_items).
+        """
+        return data <= k
+
+    @staticmethod
+    def convert_score_matrix_to_rank_matrix(S: np.ndarray) -> np.ndarray:
+        """
+        Create the rank matrix based on the input matrix S.
+
+        Parameters:
+        S (np.ndarray): The predicted score matrix of shape (n_users, n_items).
+
+        Returns:
+        np.ndarray: The matrix indicate the rank of each item for each user (n_users, n_items).
+        """
+        rank = np.zeros_like(S, dtype=np.int32)
+        ind_matrix = np.argsort(S, axis=1)[:, ::-1]
+        rank[np.arange(S.shape[0])[:, np.newaxis], ind_matrix] = np.tile(
+            np.arange(S.shape[1]), (S.shape[0], 1)
+        )
+
+        return rank
+
+    @staticmethod
+    def convert_score_matrix_to_top_item_matrix(S: np.ndarray) -> np.ndarray:
+        """
+        Calculate the user rank matrix based on the input matrix S.
+
+        Parameters:
+        S (np.ndarray): The predicted score matrix of shape (n_users, n_items).
+
+        Returns:
+        np.ndarray: The matrix indicate top items for each user (n_users, n_items).
+        """
+        return np.argsort(S, axis=1)[:, ::-1]
+
+
+if __name__ == "__main__":
     print("Test Data Conversion Strategy Pattern Design.")
 
     S = np.array([[0.1, 0.2, 0.3, 0.4, 0.5], [0.5, 0.4, 0.3, 0.2, 0.1]])
 
-    rank_matrix = create_rank_matrix(S)
-    top_item_matrix = create_top_item_matrix(S)
+    rank_matrix = DataConversion.convert_score_matrix_to_rank_matrix(S)
+    top_item_matrix = DataConversion.convert_score_matrix_to_top_item_matrix(S)
 
     print(rank_matrix)
     print(top_item_matrix)
+    print()
 
-    context = DataConversionContext(RankMatrixConversionStrategy())
+    context = DataConversion(RankMatrixConversionStrategy())
     print(context.convert_to_relevance_matrix(rank_matrix, k=2, rank_relevance=True))
 
     context.set_strategy(TopItemMatrixConversionStrategy())
-    print(context.convert_to_relevance_matrix(top_item_matrix, k=2, rank_relevance=True))
+    print(
+        context.convert_to_relevance_matrix(top_item_matrix, k=2, rank_relevance=True)
+    )
 
+    print()
     context.set_strategy(RankMatrixConversionStrategy())
     print(context.convert_to_relevance_matrix(rank_matrix, k=2, rank_relevance=False))
 
     context.set_strategy(TopItemMatrixConversionStrategy())
-    print(context.convert_to_relevance_matrix(top_item_matrix, k=2, rank_relevance=False))
+    print(
+        context.convert_to_relevance_matrix(top_item_matrix, k=2, rank_relevance=False)
+    )
--- metrics.py
+++ metrics.py
@@ -1,18 +1,27 @@
 import numpy as np
+from typing import List
 import sklearn.metrics as metrics
+
+from data import DataConversion
 
 
 class Metrics:
+    """
+    A class to calculate the metrics for evaluating the recommendation system.
+
+    Note: This class only supports the binary relevance score.
+    """
+
     def __init__(self):
         pass
 
     @staticmethod
-    def dcg_score(cls, R: np.ndarray, S: np.ndarray, k: int = 30) -> float:
+    def dcg_score(R: np.ndarray, S: np.ndarray, k: int = 30) -> float:
         """
         Calculate the discounted cumulative gain (DCG) score.
 
         Parameters:
-        R (np.ndarray): The relevance score matrix of shape (n_users, n_items).
+        R (np.ndarray): The binary relevance score matrix of shape (n_users, n_items).
         S (np.ndarray): The predicted score matrix of shape (n_users, n_items).
         k (int): The number of items to consider for each user. Default is 30.
 
@@ -22,12 +31,12 @@ class Metrics:
         return metrics.dcg_score(R, S, k=k)
 
     @staticmethod
-    def ndcg_score(self, R: np.ndarray, S: np.ndarray, k: int = 30) -> float:
+    def ndcg_score(R: np.ndarray, S: np.ndarray, k: int = 30) -> float:
         """
         Calculate the normalized discounted cumulative gain (NDCG) score.
 
         Parameters:
-        R (np.ndarray): The relevance score matrix of shape (n_users, n_items).
+        R (np.ndarray): The binary relevance score matrix of shape (n_users, n_items).
         S (np.ndarray): The predicted score matrix of shape (n_users, n_items).
         k (int): The number of items to consider for each user. Default is 30.
 
@@ -37,70 +46,111 @@ class Metrics:
         return metrics.ndcg_score(R, S, k=k)
 
     @staticmethod
-    def map_score(self, R: np.ndarray, S: np.ndarray, k: int = 30) -> float:
+    def map_score(R: np.ndarray, S: np.ndarray, k: int = 30) -> float:
         """
         Calculate the mean average precision (MAP) score.
 
         Parameters:
-        R (np.ndarray): The relevance score matrix of shape (n_users, n_items).
+        R (np.ndarray): The binary relevance score matrix of shape (n_users, n_items).
         S (np.ndarray): The predicted score matrix of shape (n_users, n_items).
         k (int): The number of items to consider for each user. Default is 30.
 
         Returns:
         float: The MAP score.
         """
-        pass
+        S = DataConversion.convert_score_matrix_to_rank_matrix(S) <= k
+        return np.mean(np.sum(S * R, axis=1) / k)
 
     @staticmethod
-    def lrap_score(self, R: np.ndarray, S: np.ndarray, k: int = 30) -> float:
+    def lrap_score(R: np.ndarray, S: np.ndarray, k: int = 30) -> float:
         """
         Calculate the label-ranking average precision (LRAP) score.
 
         Parameters:
-        R (np.ndarray): The relevance score matrix of shape (n_users, n_items).
+        R (np.ndarray): The binary relevance score matrix of shape (n_users, n_items).
         S (np.ndarray): The predicted score matrix of shape (n_users, n_items).
         k (int): The number of items to consider for each user. Default is 30.
 
         Returns:
         float: The LRAP score.
         """
-        pass
+        return metrics.label_ranking_average_precision_score(R, S)
 
     @staticmethod
-    def dcf_score(self, R: np.ndarray, S: np.ndarray, k: int = 30) -> float:
+    def dcf_score(S: np.ndarray, groups: List[np.array], k: int = 30) -> float:
         """
         Calculate the deviation from producer fairness (DCF) score.
 
         Parameters:
-        R (np.ndarray): The relevance score matrix of shape (n_users, n_items).
         S (np.ndarray): The predicted score matrix of shape (n_users, n_items).
+        groups (List[np.array]): The list of group indices.
         k (int): The number of items to consider for each user. Default is 30.
 
         Returns:
         float: The DCF score.
         """
-        pass
+        assert len(groups) == 2, "The number of groups must be 2."
+
+        B = DataConversion.convert_score_matrix_to_rank_matrix(S) <= k
+
+        cnt = np.sum(B, axis=0) / B.shape[0]
+        cnt_g = [np.sum(cnt[g]) / len(g) for g in groups]
+
+        return cnt_g[0] - cnt_g[1]
 
     @staticmethod
-    def mdg_score(self, R: np.ndarray, S: np.ndarray, k: int = 30) -> float:
+    def mdg_score_each_item(R: np.ndarray, S: np.ndarray, k: int = 30) -> float:
         """
-        Calculate the mean discounted gain (MDG) score.
+        Calculate the mean discounted gain (MDG) score for each item.
 
         Parameters:
-        R (np.ndarray): The relevance score matrix of shape (n_users, n_items).
+        R (np.ndarray): The binary relevance score matrix of shape (n_users, n_items).
         S (np.ndarray): The predicted score matrix of shape (n_users, n_items).
         k (int): The number of items to consider for each user. Default is 30.
 
         Returns:
+        float: The MDG score for each item.
+        """
+        matrix_rank = DataConversion.convert_score_matrix_to_rank_matrix(S)
+        matrix_rank = matrix_rank * (matrix_rank <= k) * R
+
+        return np.mean(np.log2(matrix_rank + 2), axis=0)
+
+    @staticmethod
+    def mdg_score(items_mdg: np.array, p: float) -> float:
+        """
+        Calculate the mean discounted gain (MDG) score.
+
+        Parameters:
+        items_mdg (np.array): The MDG score for each item.
+        p (float): The proportion of items to consider.
+
+        Returns:
         float: The MDG score.
         """
-        pass
+        n_item = items_mdg.shape[0]
+        k = int(n_item * p)
+        if k > 0:
+            partition_idx = np.argpartition(items_mdg, k)[:k]
+        else:
+            partition_idx = np.argpartition(items_mdg, k)[k:]
+
+        return np.mean(items_mdg[partition_idx])
 
 
 if __name__ == "__main__":
-    R = np.array([[0, 0, 0, 0, 2], [0, 1, 0, 1, 1]])
-    S = np.array([[0, 0.05, 0.1, 0.2, 0.3]])
-    # S = np.array([[1, 3, 2, 4, 5]])
+    R = np.array([[1, 0, 1, 0, 1], [0, 1, 0, 1, 0]])
+    S = np.array([[0.1, 0.2, 0.3, 0.4, 0.5], [0.5, 0.4, 0.3, 0.2, 0.1]])
 
-    print(metrics.dcg_score(R, S, k=2))
-    print(metrics.ndcg_score(R, S, k=2))
+    print("Test Metrics Strategy Pattern Design.")
+    print(Metrics.dcg_score(R, S))
+    print(Metrics.ndcg_score(R, S))
+
+    print(Metrics.map_score(R, S))
+    print(Metrics.lrap_score(R, S))
+
+    print(Metrics.dcf_score(S, [[0], [1]]))
+
+    print(Metrics.mdg_score_each_item(R, S))
+    print(Metrics.mdg_score(Metrics.mdg_score_each_item(R, S), 0.1))
+    print(Metrics.mdg_score(Metrics.mdg_score_each_item(R, S), -0.1))
